784bf911180f493e632a819ee91dd749c51b9cbe
atoshakiwaiyca-a... <atoshakiwaiyca@gmail.com>
2026-08-30 15:00:39 +0000

    Switch configuration back to standard Node server

--- backend/server.js
+++ backend/server.js
@@ -89,11 +89,7 @@ app.get('*', (req, res) => {
 
 // Start Server
 
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
