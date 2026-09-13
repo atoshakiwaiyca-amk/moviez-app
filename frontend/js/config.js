cc66ce2f40683990e3294a1c8b9545ff1b5bbcf9
atoshakiwaiyca-a... <atoshakiwaiyca@gmail.com>
2026-09-13 11:48:04 +0000

    Finalize Supabase database setup and config

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,4 @@
 // frontend/js/config.js
 window.API_BASE = 'https://moviez-app-api.onrender.com/api';
+window.SUPABASE_URL = 'https://bjkehtxvridkhukickrpy.supabase.co';
+window.SUPABASE_KEY = 'sb_publishable_Sv9leUjdS5NKbLALxH7a5A_E1rSKdIS'; 
